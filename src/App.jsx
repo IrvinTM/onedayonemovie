c5be5104b7c30a29cac797267f4a0b412babe2c3
IrvinTM <irvint698@gmail.com>
2024-07-11 19:29:36 -0600

    ini

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+function App() {
+  return (
+    <>
+      <div className="flex flex-col justify-center items-center gap-4">
+        <h1 className="font-extrabold text-2xl">Sputnik: extraño pasajero (2020)</h1>
+        <iframe
+          className="h-90 w-90"
+          src="https://filemoon.sx/e/4oqost3kgqto"
+          webkitallowfullscreen="true"
+          mozallowfullscreen="true"
+          allowfullscreen="true"
+        ></iframe>
+      </div>
+    </>
+  );
+}
+
+export default App;
